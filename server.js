e4b3dafe8527acd98bacdfea504a8f2f2f1f0b65
Michael Quah <michaelquah333@gmail.com>
2022-08-24 22:47:11 +0800

    Add MongoDB Atlas connection to server

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-import express from "express";
+import express from 'express';
 import cors from "cors";
 import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import * as apps from './app.js';
+
+dotenv.config();
 
 const chkPort=val=>{
     let port =parseInt(val,10);
@@ -11,16 +15,25 @@ const chkPort=val=>{
     return false;
   }
 
-let app=express();
+const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use (apps.router);
-//listen to provided port or else use port 4200
+
+// Server Port
 const port =chkPort(process.env.PORT||4200);
-app.on("listening",()=>{
-    console.log("listening on port : " + port);
+
+// Setup MongoDB Atlas Connection
+const mongo_uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.3wmjdvj.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true});
+const conn = mongoose.connection;
+conn.once("open", () => {
+  console.log("MongoDB Atlas database connected successfully");
 })
+conn.on('error', console.error.bind(console, 'MongoDB Atlas connection error'));
 
-app.listen(port);
-
-console.log("listening on port 4200");
+// Server listening port
+app.listen(port, () => {
+  console.log(`Server listening on Port ${port}`);
+});
