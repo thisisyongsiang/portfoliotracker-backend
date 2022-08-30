bed359350d68acdb5db9177a987e94e1954b693c
Michael Quah <michaelquah333@gmail.com>
2022-08-30 21:59:49 +0800

    Add env variables secrets

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import {router_user} from './router_user.js';
 import {router_portfolio} from './router_portfolio.js';
 import {financeRouter}from './financeAPI/router.js'
 
-dotenv.config();
+// dotenv.config();
 
 const chkPort=val=>{
     let port =parseInt(val,10);
@@ -32,7 +32,8 @@ app.use(financeRouter);
 const port =chkPort(process.env.PORT||4200);
 
 // Setup MongoDB Atlas Connection
-const mongo_uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.3wmjdvj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+//const mongo_uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.3wmjdvj.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+const mongo_uri = `mongodb+srv://mongo_admin:admin123@cluster0.3wmjdvj.mongodb.net/tracker?retryWrites=true&w=majority`;
 mongoose.connect(mongo_uri, {useNewUrlParser: true, useUnifiedTopology: true});
 const conn = mongoose.connection;
 conn.once("open", () => {
